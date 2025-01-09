ebfbde0b8f75e8bce75e1c659a9cac2208cdd061
jhowlacerda <jonatha.lacerda@outlook.com>
2025-01-09 00:12:27 -0300

    refactor: adjust validation cpf.

--- src/main/java/com/br/manutencao_usuario/dto/UserUpdateDto.java
+++ src/main/java/com/br/manutencao_usuario/dto/UserUpdateDto.java
@@ -2,14 +2,11 @@ package com.br.manutencao_usuario.dto;
 
 import jakarta.validation.constraints.NotNull;
 
-import java.util.UUID;
-
 public record UserUpdateDto(
-        @NotNull
-        UUID id,
         String name,
         String surname,
         String email,
         String cellPhone,
+        @NotNull
         String cpf) {
 }
