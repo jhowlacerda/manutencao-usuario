c313e22f146f69686b79907307da41677d0844d8
jhowlacerda <jonatha.lacerda@outlook.com>
2025-01-09 00:08:57 -0300

    refactor: remove classes.

--- src/main/java/com/br/manutencao_usuario/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.br.manutencao_usuario.repository;
-//TODO CRIAR O ENDEREÇO DO USUARIO
-//import com.br.manutencao_usuario.model.Address;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.UUID;
-//
-//public interface AddressRepository extends JpaRepository<Address, UUID> {
-//}
